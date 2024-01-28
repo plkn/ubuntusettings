0579927006d8aded6344b680d12b3fdfe13683b5
root <root@FRACTAL>
2024-01-28 21:04:47 +0300

    Added which-key plugin

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<Leader>fb', builtin.oldfiles, {})
-vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<Leader>fk', builtin.keymaps, {})
+vim.keymap.set('n', '<Leader>ff', builtin.find_files, {desc = "Find files"})
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, {desc = "Find buffers"})
+vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {desc = "Find in files"})
+vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {desc = "Find help"})
+vim.keymap.set('n', '<Leader>fk', builtin.keymaps, {desc = "Find keymaps"})
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -30,6 +30,15 @@ return require('packer').startup(function(use)
 					"nvim-lua/plenary.nvim",
 			},
 	})
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+      }
+    end
+  }
 
   if packer_bootstrap then
     require('packer').sync()
