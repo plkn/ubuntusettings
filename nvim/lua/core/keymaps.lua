118191a8275e500514c63e71421835c1dc6e1a01
root <root@FRACTAL>
2024-01-28 22:47:43 +0300

    Macros for todo list

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,5 +3,8 @@ vim.keymap.set('n', '<c-s>', '<cmd>write<cr>')
 vim.keymap.set('n', '<c-a>', ':keepjumps normal! ggVG<cr>')
 
 -- TODO list
-vim.keymap.set('n', '<leader>1', 'I[ ] ')
-vim.keymap.set('n', '<leader>2', '^f[lrx')
+vim.keymap.set('n','<leader>1', '@1') -- see macros.lua
+vim.keymap.set('n','<leader>2', '@2') -- see macros.lua
+
+
+
---
+++ nvim/lua/core/macros.lua
@@ -0,0 +1,4 @@
+-- TODO list
+vim.api.nvim_exec("let @1 = 'I[ ] '", false)
+vim.api.nvim_exec("let @2 = '^lrx$'", false)
+
