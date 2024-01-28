82f5a9cb5e13fd91cf263ae17c23d7debbb9a884
root <root@FRACTAL>
2024-01-28 20:08:20 +0300

    Added relativenumbers for lines.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = '\\'
 vim.g.maplocalleader = '\\'
 
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
--- nvim/lua/core/plugin_config/lazygit.lua
+++ nvim/lua/core/plugin_config/lazygit.lua
@@ -1,2 +1,1 @@
 vim.keymap.set('n', '<Leader>gg', ':LazyGit<CR>', {})
-
