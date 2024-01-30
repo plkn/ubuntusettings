6a76130bc800dd6448f2b16c0f4e56dddf386589
Pavel Koryakin <pavel.koryakin@gmail.com>
2024-01-30 09:43:55 +0300

    Add ripgrep installation to setup.sh

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+install_ripgrep() {
+  echo "Installing ripgrep..."
+  result=$(apt install ripgrep 2>&1)
+
+  if [ $? -eq 0 ]; then
+    result=""
+  else
+    echo "Failed to install ripgrep"
+    echo "$result"
+  fi
+}
+
 install_golps() {
     echo "Installing golang LSP..."
 
@@ -27,6 +39,7 @@ install_bash_lsp() {
 
 }
 
+install_ripgrep
 install_golps
 install_bash_lsp
 echo "LSPs installation complete"
