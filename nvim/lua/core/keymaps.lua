7f104fe89a058446c98cb5667decf8fa0d32941c
root <root@FRACTAL>
2024-01-31 20:16:15 +0300

    Keymaps to move lines up and down

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -6,4 +6,13 @@ vim.keymap.set('n', '<c-a>', ':keepjumps normal! ggVG<CR>')
 vim.keymap.set('n','<leader>1', '@1') -- see macros.lua
 vim.keymap.set('n','<leader>2', '@2') -- see macros.lua
 
+-- close vim
 vim.keymap.set('n', '<C-M-q>', ':qa<CR>')
+
+
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
+vim.keymap.set('v', '<A-j>', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('v', '<A-k>', ':m \'<-2<CR>gv=gv')
