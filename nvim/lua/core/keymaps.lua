facdf9c9087a1b3e70ad96917ec3c72752760b72
Pavel Koryakin <pavel.koryakin@gmail.com>
2024-01-31 19:01:49 +0300

    Map C-S-q to :qa<CR>

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,10 +1,9 @@
 vim.keymap.set('n','<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<c-s>', '<cmd>write<cr>')
-vim.keymap.set('n', '<c-a>', ':keepjumps normal! ggVG<cr>')
+vim.keymap.set('n', '<c-s>', '<cmd>write<CR>')
+vim.keymap.set('n', '<c-a>', ':keepjumps normal! ggVG<CR>')
 
 -- TODO list
 vim.keymap.set('n','<leader>1', '@1') -- see macros.lua
 vim.keymap.set('n','<leader>2', '@2') -- see macros.lua
 
-
-
+vim.keymap.set('n', '<C-M-q>', ':qa<CR>')
