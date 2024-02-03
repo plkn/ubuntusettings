ab7659239f234373617264e0be15e67428873f9a
root <root@FRACTAL>
2024-02-03 18:17:20 +0300

    Removed keymap <c-a> for select all since it's trivial

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,6 +1,5 @@
 vim.keymap.set('n','<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<c-s>', '<cmd>write<CR>')
-vim.keymap.set('n', '<c-a>', ':keepjumps normal! ggVG<CR>')
 
 -- TODO list
 vim.keymap.set('n','<leader>1', '@1') -- see macros.lua
